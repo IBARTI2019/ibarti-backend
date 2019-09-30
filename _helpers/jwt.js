91040c43c18b3931116a9b74a717e9f9db73d28f
yaviles <yaviles@grupo-ari.net>
2019-09-30 12:03:37 -0400

    Semilla

---
+++ _helpers/jwt.js
@@ -0,0 +1,34 @@
+const expressJwt = require('express-jwt');
+const config = require('config.json');
+const userService = require('../seguridad/usuario.service');
+
+module.exports = jwt;
+
+function jwt() {
+    const secret = config.secret;
+    return expressJwt({
+        secret,
+        isRevoked
+    }).unless({
+        path: [
+            // Lista de rutas que no requieren Autenticación
+            '/seguridad/usuario/authenticate',
+            '/seguridad/usuario/logout',
+            '/seguridad/usuario/isLoggedIn',
+            '/seguridad/usuario/token',
+            '/seguridad/usuario/register',
+            '/seguridad/usuario/token',
+        ]
+    });
+}
+
+async function isRevoked(req, payload, done) {
+    const user = await userService.getById(payload.sub);
+    // Rovacar Toen si el usuario no existe
+    if (!user) {
+        console.log('User Not Found');
+        return done(null, true);
+    }
+
+    done();
+};
---
+++ seguridad/usuario.controller.js
@@ -0,0 +1,98 @@
+﻿const express = require('express');
+const router = express.Router();
+const userService = require('./usuario.service');
+
+router.post('/authenticate', authenticate);
+router.post('/register', register);
+router.post('/token-refresh', gNewTokenAcces);
+router.get('/', getAll);
+router.get('/actual', getCurrent);
+router.get('/:id', getById);
+router.put('/:id', update);
+router.put('/logout/:id', logout);
+router.delete('/:id', _delete);
+
+module.exports = router;
+
+function authenticate(req, res, next) {
+    userService.isLoggedIn(req.body)
+        .then((loggedIn) => {
+            if (loggedIn) {
+                res.status(400).json({
+                    message: 'Este Usuario ya posee sesion activa'
+                });
+            } else {
+                userService.authenticate(req.body)
+                    .then((user) => user ? res.json(user) : res.status(400).json({
+                        message: 'Username or password is incorrect'
+                    }))
+                    .catch((err) => next(err));
+            }
+        })
+        .catch((err) => next(err));
+}
+
+function register(req, res, next) {
+    userService.create(req.body)
+        .then((data) => {
+            return res.json(data);
+        })
+        .catch((err) => next(err));
+}
+
+function gNewTokenAcces(req, res, next) {
+    userService.gNewTokenAcces(req.body.username, req.body.tokenRefresh)
+        .then((newTokenAcces) => {
+            if (newTokenAcces !== 'User Not Found' && newTokenAcces !== 'User Not Authorized') {
+                res.json(newTokenAcces);
+            } else if (newTokenAcces === 'User Not Found') {
+                res.status(400).json({
+                    message: newTokenAcces
+                });
+            } else {
+                res.status(401).json({
+                    message: newTokenAcces
+                });
+            }
+        })
+        .catch((err) => next(err));
+}
+
+function getAll(req, res, next) {
+    userService.getAll()
+        .then((users) => res.json(users))
+        .catch((err) => {
+            console.log('Mensaje de Error: ', err);
+            next(err);
+        });
+}
+
+function getCurrent(req, res, next) {
+    userService.getById(req.user.sub)
+        .then((user) => user ? res.json(user) : res.sendStatus(404))
+        .catch((err) => next(err));
+}
+
+function getById(req, res, next) {
+    userService.getById(req.params.id)
+        .then((user) => user ? res.json(user) : res.sendStatus(404))
+        .catch((err) => next(err));
+}
+
+function logout(req, res, next) {
+    userService.logout(req.params.id)
+        .then(() => res.json({}))
+        .catch((err) => next(err));
+}
+
+function update(req, res, next) {
+    userService.update(req.params.id, req.body)
+        .then(() => res.json({}))
+        .catch((err) => next(err));
+}
+
+function _delete(req, res, next) {
+    userService.delete(req.params.id)
+        .then(() => res.json({}))
+        .catch((err) => next(err));
+}
